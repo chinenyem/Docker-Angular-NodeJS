5d26a6fb95e6c8a6327f3138b7ababb3e9383b1b
Chinenyem <chnw1947@gmail.com>
2018-09-13 16:53:30 -0400

    this push includes dev changes

---
+++ express-server/routes/api.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const userarray = [];
+const axios = require('axios')
+
+
+// MongoDB URL from the docker-compose file
+//going to have to change this for production
+const dbHost = 'mongodb://database/mean2-docker';
+
+// Connect to mongodb
+mongoose.connect(dbHost);
+
+// create mongoose schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+// create mongoose model
+const User = mongoose.model('User', userSchema);
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+    res.send('api works');
+});
+
+/* GET all users. */
+router.get('/users', (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) res.status(500).send(error);
+        res.status(200).json(users);
+    });
+});
+
+
+/* GET one users. */
+router.get('/users/:id', (req, res) => {
+    User.findById(req.param.id, (err, users) => {
+        if (err) res.status(500).send(error)
+        res.status(200).json(users);
+    });
+});
+
+/* Create a user. */
+router.post('/users', (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        age: req.body.age
+    });
+
+    user.save(error => {
+        if (error) res.status(500).send(error);
+
+        res.status(201).json({
+            message: 'User created successfully',
+            name:req.body.name,
+            age:req.body.age
+
+        });
+    });
+});
+
+router.get('/busstations', (req, res) => {
+  axios.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y')
+    .then(response => {
+      res.status(200).json(response.data);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+router.post('/getBustationslocations',( req, res)=>{
+  let url = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" + req.body.station + "&key=MW9S-E7SL-26DU-VV8V&json=y"
+  axios.get(url)
+    .then(response => {
+      res.status(200).json(response.data);
+    })
+    .catch( error => {
+      res.status(500).send(error);
+    });
+});
+
+module.exports = router;
---
+++ express-server/server.js
@@ -0,0 +1,72 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+
+const bodyParser = require('body-parser');
+
+// Get our API routes
+const api = require('./routes/api');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// Cross Origin middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+// Set our api routes
+app.use('/', api);
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '4400';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+//server.listen(port)
+/**
+ * bind the socketIO with http server.
+ */
+
+//const io = socketIO(server);
+/**
+ * open socket IO connection.
+ */
+
+
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
+const io = require('socket.io').listen(server);
+
+// setInterval(function () {
+//   api.get("/")
+//   io.sockets.emit('users', );
+// }, 5000);
+
+ io.on('connection', (socket) => {
+    console.log('aaaaaaaa user connected');
+    socket.on('persons lists',(result) => {
+      setInterval(function(){
+        io.emit("persons lists", result);
+      }, 5000)
+
+    })
+});
